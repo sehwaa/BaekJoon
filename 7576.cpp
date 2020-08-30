c6c89055d9e5f1a8d9584dfd3b4621d09dc56551
sehwaa <nsh235482@gmail.com>
2020-08-30 14:11:37 +0900

    7576 토마토 (런타임 에러)

--- 7576.cpp
+++ 7576.cpp
@@ -8,15 +8,14 @@ int M, N;
 int **tomato = new int*[N];
 int **visited = new int*[N];
 
+queue< pair<int, int> > q;
+
 int coX[] = {-1, 1, 0, 0};
 int coY[] = {0, 0, -1, 1};
 
-void bfs(int x, int y) 
-{
-  queue<pair<int, int>> q;
-  q.push(make_pair(x,y));
-  visited[x][y] = 1;
+int answer = 0;
 
+void bfs() {
   while(!q.empty()) {
     int cx = q.front().first;
     int cy = q.front().second;
@@ -27,20 +26,20 @@ void bfs(int x, int y)
       int px = cx + coX[i];
       int py = cy + coY[i];
 
-      if (px >= 0 && py >= 0 && px < N && py < M && visited[px][py] == 0 && tomato[px][py] != -1) {
+      if (px >= 0 && py >= 0 && px < N && py < M && visited[px][py] == 0 && tomato[px][py] == 0) {
         visited[px][py] = visited[cx][cy] + 1;
         q.push(make_pair(px, py));
+        answer = visited[px][py];
       }
     }
   }
 }
 
-int main()
-{
+int main() {
   cin >> M >> N;
 
   int value;
-  vector< pair<int, int> > ripeTomato;
+
   for (int i = 0; i < N; i++) {
     tomato[i] = new int[M];
     visited[i] = new int[M];
@@ -48,19 +47,25 @@ int main()
       cin >> value;
       tomato[i][j] = value;
       visited[i][j] = 0;
-      if (value == 1) ripeTomato.push_back(make_pair(i, j));
+      if (value == 1) {
+        q.push(make_pair(i, j));
+        visited[i][j] = 1;
+      }
     }
   }
 
-  for (int i = 0; i < ripeTomato.size(); i++) {
-    bfs (ripeTomato.at(i).first, ripeTomato.at(i).second);
-  }
+  bfs ();
 
   for (int i = 0; i < N; i++) {
-    delete []tomato[i];
+    for (int j = 0; j < M; j++) {
+      if (visited[i][j] == 0 && tomato[i][j] == 0) {
+        answer = -1;
+      }
+    }
   }
 
-  delete []tomato;
-
+  if (answer == 0 || answer == -1) cout << answer;
+  else  cout << answer - 1;
+  
   return 0;
 }
