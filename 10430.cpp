e5e36fb1dc88921b2d574b6a4b027ee7262193ca
sehwaa <nsh235482@gmail.com>
2020-09-03 00:00:00 +0900

    10430 나머지

---
+++ 10430.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int A, B, C;
+  cin >> A >> B >> C;
+
+  if (C != 0) {
+    cout << (A + B) % C << endl;
+    cout << ((A%C) + (B%C))%C << endl;
+    cout << (A*B)%C << endl;
+    cout << ((A%C) * (B%C))%C << endl;
+  }
+  return 0;
+}
