3a531f7c935d1aa9649ce6ee1326e710b23478c0
sehwaa <nsh235482@gmail.com>
2020-09-10 22:00:51 +0900

    2206 벽 부수고 이동하기 (백준에서 제공하는 테스트케이스는 맞았음)

--- 2206.cpp
+++ 2206.cpp
@@ -5,48 +5,49 @@
 using namespace std;
 
 int N, M;
-int map[1001][1001];
-int visited[1001][1001];
+int **map;
+int **visited;
 
 int coX[] = {-1, 1, 0, 0};
 int coY[] = {0, 0, -1, 1};
 
-int breakWall = 0;
-
 int bfs(int x, int y) {
-  queue< pair<int, int> > q;
-  q.push(make_pair(x, y));
-  visited[x][y] = 1;
+  queue< pair< pair<int, int>, int > > q;
+  q.push(make_pair(make_pair(x,y), 1));
+  visited[0][0] = 1;
 
   while (!q.empty()) {
-    int cx = q.front().first;
-    int cy = q.front().second;
-
+    int cx = q.front().first.first;  
+    int cy = q.front().first.second;
+    int wall = q.front().second;
     q.pop();
-
-    if (cx == N && cy == M) return visited[cx][cy];
+    
+    if (cx == N-1 && cy == M-1) return visited[N-1][M-1];
 
     for (int i = 0; i < 4; i++) {
       int px = cx + coX[i];
       int py = cy + coY[i];
-
-      if (px >= 0 && py >=0 && visited[px][py] == 0) {
-        if (map[cx][cy] ==1 && map[px][py] == 0) {
-          q.push(make_pair(px, py));
-          if (breakWall == 0) breakWall++;
-        }
+      int breakwall = wall;
+      
+      if (px >= 0 && py >= 0 && px < N && py < M && !visited[px][py] && breakwall >= 0) {
+        if (map[px][py] == 1) breakwall--;
+        q.push(make_pair(make_pair(px, py), breakwall));
         visited[px][py] = visited[cx][cy] + 1;
       }
     }
   }
-
   return -1;
 }
 
 int main() {
   cin >> N >> M;
 
+  map = new int *[N];
+  visited = new int *[N];
+
   for (int i = 0; i < N; i++) {
+    map[i] = new int[M];
+    visited[i] = new int[M];
     for (int j = 0; j < M; j++) {
       scanf("%1d", &map[i][j]);
       visited[i][j] = 0;
