6e78186cb50703366acb54f9e5293618feb9e6f4
sehwaa <nsh235482@gmail.com>
2020-09-10 23:30:13 +0900

    2206 벽 부수고 이동하기

--- 2206.cpp
+++ 2206.cpp
@@ -5,34 +5,39 @@
 using namespace std;
 
 int N, M;
-int **map;
-int **visited;
+int map[1001][1001];
+int visited[1001][1001][2];
 
 int coX[] = {-1, 1, 0, 0};
 int coY[] = {0, 0, -1, 1};
 
 int bfs(int x, int y) {
   queue< pair< pair<int, int>, int > > q;
-  q.push(make_pair(make_pair(x,y), 1));
-  visited[0][0] = 1;
+
+  q.push(make_pair(make_pair(x,y), 0));
+  visited[0][0][0] = 1;
 
   while (!q.empty()) {
-    int cx = q.front().first.first;  
+    int cx = q.front().first.first;
     int cy = q.front().first.second;
     int wall = q.front().second;
     q.pop();
     
-    if (cx == N-1 && cy == M-1) return visited[N-1][M-1];
+    if (cx == N-1 && cy == M-1) return visited[N-1][M-1][wall];
 
     for (int i = 0; i < 4; i++) {
       int px = cx + coX[i];
       int py = cy + coY[i];
-      int breakwall = wall;
       
-      if (px >= 0 && py >= 0 && px < N && py < M && !visited[px][py] && breakwall >= 0) {
-        if (map[px][py] == 1) breakwall--;
-        q.push(make_pair(make_pair(px, py), breakwall));
-        visited[px][py] = visited[cx][cy] + 1;
+      if (px >= 0 && py >= 0 && px < N && py < M && visited[px][py][wall] == 0) {
+        if (map[px][py] == 0) {
+          visited[px][py][wall] = visited[cx][cy][wall] + 1; 
+          q.push(make_pair(make_pair(px, py), wall)); 
+        }
+        else if (map[px][py] == 1 && wall == 0) {
+          visited[px][py][1] = visited[cx][cy][wall] + 1; 
+          q.push(make_pair(make_pair(px, py), 1)); 
+        }
       }
     }
   }
@@ -42,15 +47,9 @@ int bfs(int x, int y) {
 int main() {
   cin >> N >> M;
 
-  map = new int *[N];
-  visited = new int *[N];
-
   for (int i = 0; i < N; i++) {
-    map[i] = new int[M];
-    visited[i] = new int[M];
     for (int j = 0; j < M; j++) {
       scanf("%1d", &map[i][j]);
-      visited[i][j] = 0;
     }
   }
 
