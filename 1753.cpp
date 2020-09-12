3c82afdc67fa881ec347f3cea459cdbba1c2f268
sehwaa <nsh235482@gmail.com>
2020-09-13 01:04:36 +0900

    1753 최단경로 (시간 초과)

---
+++ 1753.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+#define MAX_V 300000
+int V, E;
+vector< pair<int, int> > adj[MAX_V];
+
+vector< int > dijkstra(int src) {
+  vector<int> dist(V+1, 11);
+  dist[src] = 0;
+  priority_queue< pair<int, int>, vector< pair<int, int> >, greater<pair<int, int> > > pq;
+  pq.push(make_pair(0, src));
+
+  while(!pq.empty()) {
+    int cost = pq.top().first;
+    int here = pq.top().second;
+    pq.pop();
+
+    if (dist[here] < cost) continue;
+
+    for (int i = 0; i < adj[here].size(); i++) {
+      int there = adj[here][i].first;
+      int nextDist = cost + adj[here][i].second;
+
+      if (dist[there] > nextDist) {
+        dist[there] = nextDist;
+        pq.push(make_pair(nextDist, there));
+      }
+    }
+  }
+  return dist;
+}
+
+int main() {
+  int start;
+  cin >> V >> E;
+  cin >> start;
+  
+  for (int i = 0; i < E; i++) {
+    int u,v,w;
+    cin >> u >> v >> w;
+    adj[u].push_back(make_pair(v, w));
+  }
+
+  vector<int> result = dijkstra(start);
+
+  for(int i = 1; i <= V; i++) {
+    if (result[i] == 11) cout << "INF" << endl;
+    else cout << result[i] << endl;
+  }
+  return 0;
+}
