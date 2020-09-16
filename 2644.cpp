457fe4754e3c45c8f083a77a7cce7385cf4cf20f
sehwaa <nsh235482@gmail.com>
2020-09-16 22:29:21 +0900

    2644 촌수 계산(미완)

--- 2644.cpp
+++ 2644.cpp
@@ -6,9 +6,18 @@ using namespace std;
 #define MAX 101
 int n,m;
 vector<int> relationship[MAX];
-
-void dfs(int p1, int p2) {
-  
+bool visited[MAX];
+int answer = 0;
+int dfs(int p1, int p2) {
+  cout << "p1: " << p1 << endl;
+  visited[p1] = true;
+  for (int i = 0; i < relationship[p1].size(); i++) {
+    if (relationship[p1][i] == p2) break;
+    if (!visited[relationship[p1][i]]) {
+      dfs(relationship[p1][i], p2);
+    }
+  }
+  return answer++;
 }
 
 int main() {
@@ -24,6 +33,11 @@ int main() {
     cin >> parent >> child;
     relationship[parent].push_back(child);
     relationship[child].push_back(parent);
+    visited[parent] = false;
+    visited[child] = false;
   }
+
+  dfs(p1, p2);
+  cout << answer;
   return 0;
 }
