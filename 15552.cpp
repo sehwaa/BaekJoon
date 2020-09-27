a845b47dff1ba8ea2a9810188e9fc6075f678c9f
sehwaa <nsh235482@gmail.com>
2020-09-27 14:42:55 +0900

    15552 빠른 A+B

---
+++ 15552.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int T;
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin >> T;
+    
+    for (int i = 0; i < T; i++) {
+        int A, B;
+        cin >> A >> B;
+        cout << A + B << "\n";
+    }
+    return 0;
+}
