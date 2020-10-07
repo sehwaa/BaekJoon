a92a69d8053d602530028b7da2902a187ac92c9f
sarah <nsh235482@gmail.com>
2020-10-07 23:54:05 +0900

    2026 소풍(미완)

--- 2026.cpp
+++ 2026.cpp
@@ -1,120 +1,78 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <algorithm>
 
 using namespace std;
 
 #define MAX 901
+#define FRIEND1 0
+#define FRIEND2 1
+
 int K, N, F;
-vector<int> groups[MAX];
+bool relationship[MAX][MAX];
 bool visited[MAX][MAX];
-int fixIndex = 0;
-vector<int> k_friends[MAX];
-int answer = 0;
+vector<int> group;
+int cnt = 1;
 
-void search(int f) {
-	cout << "search in f: " << f << endl;
-	cout << "search in fixIndex: " << fixIndex << endl;
-	visited[fixIndex][f] = true;
-
-	//중복일 때 count...세야함...
-	//그리고 로직 다시 짜야할듯... (비교를 이렇게게하면 처음에
-	//만들어진 애들 4명만 답이 나올수 밖에 없음)
-	//왜냐하면, k_friends랑 groups[f]랑 비교하니까 전부다 일치할수가 없음
-	//완전탐색으로 다 돌면서 체크하는 로직으로 해야할듯
-	int cnt = 1;
-	for (vector<int>::iterator it = k_friends[fixIndex].begin(); it != k_friends[fixIndex].end(); it++) {
-		bool exist = false;
-		for (vector<int>::iterator it2 = groups[f].begin(); it2 != groups[f].end(); it2++) {
-			if (*it == *it2) {
-				exist = true;
-			}
-			if (exist) {
-				cnt++;
-				break;
-			}
+int findFriends(int friend1, int friend2) {
+	visited[friend1][friend2] = true;
+	
+	if (group.size() == K) {
+		sort(group.begin(), group.end());
+		for (int i = 0; i < group.size(); i++) {
+			cout << group[i] << endl;
 		}
+		return 1;
 	}
 	
-	cout << "cnt : " << cnt << endl;
-	if (cnt != k_friends[fixIndex].size()) {
-		k_friends[fixIndex].pop_back();
-		return;
+	if (group.size() == 0) {
+		//이거 두개는 무조건 들어가서 얘네돌고도 또 돌게 만들어야 할듯
+		group.push_back(friend1);
+		group.push_back(friend2);
 	} else {
-	}
-	
-	if (k_friends[fixIndex].size() == K){
-		sort(k_friends[fixIndex].begin(), k_friends[fixIndex].end());
-		if (answer != 0) {
-			for (int i = 0; i < K; i++) {
-				cout << "answer : " << answer << endl;
-				if (k_friends[answer][i] >= k_friends[fixIndex][i]) {
-					cout << "fixIndex : " << fixIndex << endl;
-					answer = fixIndex;
-					break;
+		for (int i = 0; i < 2; i++) {
+			int count = 0;
+			for (int j = 0; j < group.size(); j++) {
+				if (i == FRIEND1) {
+					if (group[j] == friend1 || !relationship[friend1][group[j]] && !relationship[group[j]][friend1]) {
+						count = -1;
+						break;
+					}
+				} else if (i == FRIEND2) {
+					if (group[j] == friend2 || !relationship[friend2][group[j]] && !relationship[group[j]][friend2]) {
+						count = -1;
+						break;
+					}
 				}
 			}
-		}
-		else {
-			answer = fixIndex;
-			cout << "answer: " << answer << endl;
-			cout << "fixIndex: " << fixIndex << endl;
+			if (count != -1 && i == FRIEND1) group.push_back(friend1);
+			else if (count != -1 && i == FRIEND2) group.push_back(friend2);
 		}
 		
-		//erase
-		for (int i = 0; i < K; i++) {
-			cout << k_friends[answer][i] << endl;
-		}
-		return;
 	}
-
-	for (vector<int>::iterator it = groups[f].begin(); it != groups[f].end(); it++) {
-		if (!visited[fixIndex][*it]) {
-			k_friends[fixIndex].push_back(*it);
-			search(*it);
-		}
-	}
-	
-	return;
+	return -1;
 }
 
 int main() {
 	cin >> K >> N >> F;
-
+	
 	for (int i = 0; i < F; i++) {
-		int f1, f2;
-		cin >> f1 >> f2;
-		groups[f1].push_back(f2);
-		groups[f2].push_back(f1);
-	}
-
-	//erase
-	for (int i = 1; i <= N; i++) {
-		cout << "=============" << endl;
-		cout << i << " : ";
-		for (int j = 0; j < groups[i].size(); j++) {
-			cout << groups[i][j];
-		}
-		cout << endl;
+		int friend1, friend2;
+		cin >> friend1 >> friend2;
+		relationship[friend1][friend2] = true;
+		relationship[friend2][friend1] = true;
 	}
 	
+	int result = 0;
 	for (int i = 1; i <= N; i++) {
-		fixIndex = i;
-		for (int j = 0; j < N; j++) {
-			if (!visited[fixIndex][j]) {
-				k_friends[fixIndex].push_back(j+1);
-				search(j+1);
+		for (int j = 1; j <= N; j++) {
+			if (!visited[i][j] && relationship[i][j]) {
+				result = findFriends(i,j);
+				if (result == 1) return 0;
 			}
 		}
 	}
 	
-	if (answer != 0) {
-		for (int i = 0; i < K; i++) {
-			cout << k_friends[answer][i] << endl;
-		}
-		return 0;
-	}
 	cout << -1;
 	return 0;
 }
