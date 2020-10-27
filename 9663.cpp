3f3d47b950e9bbdcdc79c16fc2aca2ccc5951b89
Sarah Na <sehwaa@users.noreply.github.com>
2020-10-27 21:46:16 +0900

    9663 N-Queen(시간초과)

--- 9663.cpp
+++ 9663.cpp
@@ -1,56 +1,46 @@
 #include <iostream>
-#include <stack>
 
 using namespace std;
 
 #define MAX 15
 
 int N;
-int coX[8] = {-1, 1, 0, 0, 1, -1, 1, -1};
-int coY[8] = {0, 0, -1, 1, -1, 1, 1, -1};
-int queenCnt = 0;
 int caseCnt = 0;
-int queen[MAX][MAX];
-bool visited[MAX][MAX];
-stack< pair<int, int> > s;
+int queenCnt = 0;
+int cnt = 0;
+int chess[MAX*MAX][MAX][MAX];
+int coX[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+int coY[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+int findQueen(int x, int y) {
+	for (int i = 1; i < N; i++) {
+		for (int j = 0; j < 8; j++) {
+			int px = x + (coX[j] * i);
+			int py = y + (coY[j] * i);
+			if (px >= 0 && px < N && py >= 0 && py < N && chess[cnt][px][py] == 1) {
+				return -1;
+			}
+		}
+	}
+	return 0;
+}
 
 void attack(int x, int y) {
+	queenCnt++;
+	chess[cnt][x][y] = 1;
 	
-	for (int i = 0; i < 8; i++) {
-		int px = x + coX[i];
-		int py = y + coY[i];
-		
-		if (px >= 0 && py >= 0 && px < N && py < N) {
-			if (queen[px][py] == 1) {
-				return;
+	for (int i = x+1; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (j == y) continue;
+			int result = findQueen(i, j);
+			if (result == 0) {
+				attack(i, j);
+				if (queenCnt == N) caseCnt++;
+				queenCnt--;
+				chess[cnt][i][j] = 0;
 			}
 		}
 	}
-	
-	queen[x][y] = 1;
-	s.push(make_pair(x, y));
-	
-	if (s.size() == N) {
-		caseCnt++;
-		s.pop();
-		queen[x][y] = 0;
-		return;
-	}
-	
-	while(!s.empty()) {
-		for (int i = x; i < N; i++) {
-			for (int j = y; j < N; j++) {
-				if (i == x && i == y) continue;
-				else attack(i, j);
-			}
-		}
-		
-		if (s.size() > 0) {
-			s.pop();
-			queen[x][y] = 0;
-		}
-	}
-	return;
 }
 
 int main() {
@@ -58,7 +48,9 @@ int main() {
 	
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			attack(i, j);
+			attack(i,j);
+			queenCnt = 0;
+			cnt++;
 		}
 	}
 	
