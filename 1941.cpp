0b8e62e1270b4f5a89ed9e383253efb88b61909e
sarah <nsh235482@gmail.com>
2020-11-11 23:12:57 +0900

    1941 소문난 칠공주 (미완)

---
+++ 1941.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+char sit[5][5];
+bool visited[5][5];
+int coX[4] = {-1, 1, 0, 0};
+int coY[4] = {0, 0, -1, 1};
+int som = 0; int yeon = 0;
+int answer = 0;
+int sevenCnt = 1;
+ 
+void dfs(int x, int y) {
+	cout << "x: " << x << " y: " << y << endl;
+	cout << "som: " << som << " yeon: " << yeon << endl;
+	visited[x][y] = true;
+ 
+	if (som + yeon == 7) {
+		answer++;
+		return;
+	}
+ 
+	for (int i = 0; i < 4; i++) {
+		int px = x + coX[i];
+		int py = y + coY[i];
+ 
+		if (px >= 0 && py >= 0 && px < 5 && py < 5 && !visited[px][py]) {
+			if (sit[px][py] == 'S') som++;
+			else yeon++;
+ 
+			if (yeon >= 4) {
+				yeon--;
+			} else {
+				dfs (px, py);
+                if (sit[px][py] == 'S') som--;
+	            else yeon--;
+			}
+		}
+	}
+	
+	visited[x][y] = false;
+}
+ 
+int main() {
+ 
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			cin >> sit[i][j];
+			visited[i][j] = false;
+		}
+	}
+ 
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (sit[i][j] == 'S') som++;
+			else yeon++;
+			dfs(i,j);
+			som = 0; yeon = 0;
+		}
+	}
+	
+	cout << answer;
+	return 0;
+}
