39e89545f75920f871c3218732dcaef7906612ae
Sarah Na <sehwaa@users.noreply.github.com>
2021-01-30 22:11:43 +0900

    2231 분해합

---
+++ 2231.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int N, constructor;
+	cin >> N;
+	int minM = 0;
+	
+	for (int i = 0; i <= N; i++) {
+		string strI = to_string(i);
+		int M = i;
+		for (int j = 0; j < strI.size(); j++) {
+			M += strI[j] - '0';
+		}
+		if (M == N) {
+			minM = i;
+			break;
+		}
+	}
+	
+	cout << minM;
+	return 0;
+}
