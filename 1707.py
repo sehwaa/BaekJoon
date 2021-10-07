737d01f05f808640ad5639ed915777541ddbbcaa
Sarah Na <nsh235482@gmail.com>
2021-10-07 23:40:09 +0900

    소스코드 길이 줄임

--- 1707.py
+++ 1707.py
@@ -1,34 +1,32 @@
 K = int(input())
-    
+
 for _ in range(K):
     V, E = map(int, input().split())
-    
+
     nodes = [[] for _ in range(V)]
-    visited = [False] * V
-    colors = {i:'r' for i in range(V)}
-    
+    visited = [1] + [0] * (V-1)
+
     for _ in range(E):
-        u,v = map(int, input().split())
-        nodes[u-1].append(v-1)
-        nodes[v-1].append(u-1)
-        
+        u, v = map(int, input().split())
+        nodes[u - 1].append(v - 1)
+        nodes[v - 1].append(u - 1)
+
+
     def search(node_idx):
-     visited[node_idx] = True
         for v in nodes[node_idx]:
-            if visited[v] == False:
-                if colors[node_idx] == 'r':
-                    colors[v] = 'b'
-                search(v)
-                
-    search(0)
-    
-    def result():
-        for idx, adj in enumerate(nodes):
-            for n in adj:
-                if colors[idx] == colors[n]:
-                    print('NO')
-                    return
-        print('YES')
-        return
-    
-    result()
+            if visited[v] == 0:
+                if visited[node_idx] == 1:
+                    visited[v] = 2
+                elif visited[node_idx] == 2:
+                    visited[v] = 1
+                s = search(v)
+                if not s:
+                    return False
+            elif visited[node_idx] == visited[v]:
+                return False
+        return True
+
+    result = search(0)
+    print("YES") if result == True else print("NO")
+
+
